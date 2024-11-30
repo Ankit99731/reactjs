bbd367727ee8f8a6b40b19a0184bc5e613717423
Ankit kumar <ankitkumarakb002@gmail.com>
2024-11-30 15:22:06 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,39 @@
+
+const Parent = React.createElement('div',{
+    id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag")
+    ]),
+]);
+
+
+console.log(Parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(Parent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading = React.createElement('h1', {
+//     id:"heading"
+// },"hi bros how are you");
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
